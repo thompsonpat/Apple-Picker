3dd58d728cd5cbc7da929ef8708eb4daacf22a79
thompsonpat <thompsonpat21@gmail.com>
2018-09-26 23:35:37 -0400

    Basic Game Setup

---
+++ Apple Picker/Assets/AppleTree.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AppleTree : MonoBehaviour {
+
+	// Prefab for instantiating apples
+	public GameObject applePrefab;
+
+	// Speed at which the AppleTree moves
+	public float speed = 1f;
+
+	// Distance where AppleTree turns around
+	public float leftAndRightEdge = 10f;
+
+	// Chance that the AppleTree will change directions
+	public float chanceToChangeDirections = 0.1f;
+
+	// Rate at which Apples will be instantiated
+	public float secondsBetweenAppleDrops = 1f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		// Basic Movement
+		Vector3 pos = transform.position;
+		pos.x += speed * Time.deltaTime;
+		transform.position = pos;
+		// Changing Direction
+	}
+}
