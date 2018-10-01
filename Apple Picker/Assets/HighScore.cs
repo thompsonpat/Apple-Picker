19fdca6a7554568e8085db992abaca9eab14ca82
thompsonpat <thompsonpat21@gmail.com>
2018-09-30 23:48:41 -0400

    save high score in PlayerPrefs

--- Apple Picker/Assets/HighScore.cs
+++ Apple Picker/Assets/HighScore.cs
@@ -14,10 +14,28 @@ public class HighScore : MonoBehaviour
 
     }
 
+    void Awake()
+    {
+        // If the PlayerPrefs HighScore already exists, read it
+        if (PlayerPrefs.HasKey("HighScore"))
+        {
+            score = PlayerPrefs.GetInt("HighScore");
+        }
+
+        // Assign the high score to HighScore
+        PlayerPrefs.SetInt("HighScore", score);
+    }
+
     // Update is called once per frame
     void Update()
     {
         Text gt = this.GetComponent<Text>();
         gt.text = "High Score: " + score;
+
+		// Update the PlayerPrefs HighScore if necessary
+		if (score > PlayerPrefs.GetInt("HighScore"))
+		{
+			PlayerPrefs.SetInt("HighScore", score);
+		}
     }
 }
