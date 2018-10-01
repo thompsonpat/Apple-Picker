763c89ff3c311c28dc65c2e37bf42faf87176e4b
thompsonpat <thompsonpat21@gmail.com>
2018-09-30 22:54:38 -0400

    catching apples

--- Apple Picker/Assets/Basket.cs
+++ Apple Picker/Assets/Basket.cs
@@ -2,27 +2,40 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Basket : MonoBehaviour {
+public class Basket : MonoBehaviour
+{
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		// Get the current screen position of the mouse from Input
-		Vector3 mousePos2D = Input.mousePosition;
+    // Use this for initialization
+    void Start()
+    {
 
-		// The camera's z position sets how far to push the mouse into 3D
-		mousePos2D.z = -Camera.main.transform.position.z;
+    }
 
-		// Convert the point from 2D screen space into 3D game world space
-		Vector3 mousePos3D = Camera.main.ScreenToWorldPoint(mousePos2D);
+    // Update is called once per frame
+    void Update()
+    {
+        // Get the current screen position of the mouse from Input
+        Vector3 mousePos2D = Input.mousePosition;
 
-		// Move the x position of this Basket to the x position of the Mouse
-		Vector3 pos = this.transform.position;
-		pos.x = mousePos3D.x;
-		this.transform.position = pos;
-	}
+        // The camera's z position sets how far to push the mouse into 3D
+        mousePos2D.z = -Camera.main.transform.position.z;
+
+        // Convert the point from 2D screen space into 3D game world space
+        Vector3 mousePos3D = Camera.main.ScreenToWorldPoint(mousePos2D);
+
+        // Move the x position of this Basket to the x position of the Mouse
+        Vector3 pos = this.transform.position;
+        pos.x = mousePos3D.x;
+        this.transform.position = pos;
+    }
+
+    void OnCollisionEnter(Collision coll)
+    {
+        // Find out what hit this basket
+        GameObject collidedWith = coll.gameObject;
+        if (collidedWith.tag == "Apple")
+        {
+            Destroy(collidedWith);
+        }
+    }
 }
