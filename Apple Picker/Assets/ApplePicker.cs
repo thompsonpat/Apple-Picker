882b410a62f5a82bf226d6da27ca92393267fc1c
thompsonpat <thompsonpat21@gmail.com>
2018-09-30 23:35:35 -0400

    destroy a basket when an apple goes offscreen

--- Apple Picker/Assets/ApplePicker.cs
+++ Apple Picker/Assets/ApplePicker.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class ApplePicker : MonoBehaviour
 {
@@ -10,10 +11,12 @@ public class ApplePicker : MonoBehaviour
     public int numBaskets = 3;
     public float basketBottomY = -14f;
     public float basketSpacingY = 2f;
+    public List<GameObject> basketList;
 
     // Use this for initialization
     void Start()
     {
+        basketList = new List<GameObject>();
         // Creates baskets spaced apart
         for (int i = 0; i < numBaskets; i++)
         {
@@ -21,6 +24,7 @@ public class ApplePicker : MonoBehaviour
             Vector3 pos = Vector3.zero;
             pos.y = basketBottomY + (basketSpacingY * i);
             tBasketGO.transform.position = pos;
+            basketList.Add(tBasketGO);
         }
     }
 
@@ -38,5 +42,14 @@ public class ApplePicker : MonoBehaviour
         {
             Destroy(tGO);
         }
+
+		// Destroy one of the baskets
+		// Get the index of the last Basket in basketList
+		int basketIndex = basketList.Count - 1;
+		// Get a reference to that Basket GameObject
+		GameObject tBasketGO = basketList[basketIndex];
+		// Remove the Basket from the list and destroy the GameObject
+		basketList.RemoveAt(basketIndex);
+		Destroy(tBasketGO);
     }
 }
